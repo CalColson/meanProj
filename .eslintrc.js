3351d9ab44efbc433940392f43582a83c092e3ee
Cal Colson <cpcolson@yahoo.com>
2017-10-08 18:07:21 -0500

    finish walking skeleton

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    'extends': 'standard',
+
+    'rules': {
+        'semi': ['error', 'always'],
+        'quotes': ['error', 'single'],
+        'indent': ['error', 4],
+        'space-before-function-paren': ['error', 'never'],
+        'no-unused-vars': 1,
+
+        'no-path-concat': 0,
+        'no-multiple-empty-lines': 0,
+        'eol-last': 0,
+    }
+
+};
+
